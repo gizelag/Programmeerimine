e86eaef9aabc3ac31b11364a5d6f162ea40a0559
gunin <gizela@taltech.ee>
2025-09-15 15:41:17 +0300

    vanus

--- week2/vanus.py
+++ week2/vanus.py
@@ -1,5 +1,5 @@
-vanus = int (input("Sisesta oma vanus:21"))
-            if vanus < 0: print ("Vigane vanus")
-            elif vanus < 18: print ("Oled veel liiga noor")
-            elif vanus 18 <= 65: print ("Oled tööeas")
-            else print ("Oled pensionieas")
+vanus = int(input("Sisesta oma vanus: "))#küsib kasutajalt vanust, muudab sisendi täisarvuks
+            if vanus < 0: print ("Vigane vanus")#kui vanus on negatiivne on vigane vanus
+            elif vanus < 18: print ("Oled veel liiga noor")#kui vanus on väiksem kui 18, siis oled veel liiga noor
+            elif vanus <= 65: print ("Oled tööeas")#kui vanus on 18 kuni 65 kaasa arvatud, siis oled tööeas
+            else print ("Oled pensionieas")#kõikidel muudel juhtudel oled pensionieas
