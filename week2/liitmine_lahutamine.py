ba744b70ab2474de1ad224b030f20b08c35016aa
gunin <gizela@taltech.ee>
2025-09-16 16:28:27 +0300

    liitmine_lahutamine.py

--- week2/liitmine_lahutamine.py
+++ week2/liitmine_lahutamine.py
@@ -1,6 +1,9 @@
 x = int(input("sisesta esimene taisarv:"))#kusib esimest taisarvu
 y = int(input("sisesta teine taisarv"))#kusib teist taisarvu
 summa = x + y#kahe arvu summa arvutamine
-if summa > 100: summa -= 50 print("loppsumma:", summa )#kui summa suurem kui 100 siis lahutab 50
-elif summa < 50: summa *= 2 print("loppsumma", summa)#kui summa alla 50-ne siis korrutab kahega
-else: print("loppsumma:", summa)#kui summa on vahemikus 50-100 valjastab lihtsalt sisestatud arvu
+if summa > 100:
+    print("loppsumma:", summa - 50)#kui summa suurem kui 100 siis lahutab 50
+elif summa < 50:
+    print("loppsumma", summa * 2)#kui summa alla 50-ne siis korrutab kahega
+else:
+    print("loppsumma:", summa)#kui summa on vahemikus 50-100 valjastab lihtsalt sisestatud arvu
