b64e7c53648c2df41f73962a953389de4c981a1e
gunin <gizela@taltech.ee>
2025-09-16 16:21:55 +0300

    liitmine_lahutamine.py

--- week2/kolmnurk.py
+++ week2/kolmnurk.py
@@ -1,7 +1,9 @@
 y = int(input("sisesta esimese kulje pikkus:"))#kusib kasutajalt esimest arvu
 z = int(input("sisesta teise kulje pikkus:"))#kusib kasutajalt teist arvu
 c = int(input("sisesta kolmanda kulje pikkus:"))#kusib kasutajalt kolmandat arvu
-if y + z > c and c + z > y and c + y > z: print("sellest saab moodustada kolmnurga", {kolmnurga_umbermoot}) = y + z + c print("kolmnurga umbermoot:", umbermoot)
+kolmnurga_umbermoot = c + y + z
+if y + z > c and c + z > y and c + y > z:
+    print(f"sellest saab moodustada kolmnurga, kolmnurga umbermoot on: {kolmnurga_umbermoot} ")
 #kui kaks kulje pikkust on suuremad kui kolmas siis kehtib ja arvutab kolmnurga umbermoodu
-else: print("sellest ei saa moodustada kolmnurka")#kui ulaltoodu ei kehti valjastab selle sonumi
-
+else:
+    print("sellest ei saa moodustada kolmnurka")#kui ulaltoodu ei kehti valjastab selle sonumi
