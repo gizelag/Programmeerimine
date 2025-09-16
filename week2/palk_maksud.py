5f3d4a67241f36422f34576bcd456125a84b4e85
gunin <gizela@taltech.ee>
2025-09-16 17:57:32 +0300

    t

--- week2/palk_maksud.py
+++ week2/palk_maksud.py
@@ -7,9 +7,9 @@ netopalk = brutopalk - tulumaks - toostuskindlustusmaks
 
 
 if komakohad == 1:
-    print(f"Palgaarvestuse kokkuvõte: \n Brutopalk: {brutopalk:.1f}€\n Tulumaks (22%): {tulumaks:.1f}€ \n Sotsiaalmaks (33%): {sotsiaalmaks:.1f}€ (tööandja maksab) \n Tööstuskindlustus (1.6%): {toostuskindlustusmaks:.1f}€ \n Netopalk (kätte saadav summa): {netopalk:.1f}€ ")
+    print(f"Palgaarvestuse kokkuvõte: \n Brutopalk: {brutopalk:.1f} €\n Tulumaks (22%): {tulumaks:.1f} € \n Sotsiaalmaks (33%): {sotsiaalmaks:.1f} € (tööandja maksab) \n Tööstuskindlustus (1.6%): {toostuskindlustusmaks:.1f} € \n Netopalk (kätte saadav summa): {netopalk:.1f} € ")
 if komakohad == 2:
-    print(f"Palgaarvestuse kokkuvõte: \n Brutopalk: {brutopalk:.2f}€\n Tulumaks (22%): {tulumaks:.2f}€ \n Sotsiaalmaks (33%): {sotsiaalmaks:.2f}€ (tööandja maksab) \n Tööstuskindlustus (1.6%): {toostuskindlustusmaks:.2f}€ \n Netopalk (kätte saadav summa): {netopalk:.2f}€ ")
+    print(f"Palgaarvestuse kokkuvõte: \n Brutopalk: {brutopalk:.2f} €\n Tulumaks (22%): {tulumaks:.2f} € \n Sotsiaalmaks (33%): {sotsiaalmaks:.2f} € (tööandja maksab) \n Tööstuskindlustus (1.6%): {toostuskindlustusmaks:.2f} € \n Netopalk (kätte saadav summa): {netopalk:.2f} € ")
 
 
 
