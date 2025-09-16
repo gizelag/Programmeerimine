83fff038df92553cdfa1250f7989013b1b026b70
gunin <gizela@taltech.ee>
2025-09-16 22:41:14 +0300

    kl

---
+++ week2/teatripileti_hind.py
@@ -0,0 +1,21 @@
+vanus = input("Siseta oma vanus: ")
+if vanus == false:
+    print("Vanus peab olema täisarv!")
+    elif vanus <= 0:
+print("Vanus ei saa olla 0 või negatiivne!")
+
+hind = 0
+if vanus >= 1 and vanus <= 5:
+    hind = 0
+    if vanus >= 6 and vanus <= 17:
+        hind = 8
+        if vanus >= 18 and vanus <= 64:
+            hind = 12
+            if vanus >= 64
+                hind = 7
+if vanus >= 6 and vanus  <= 17 and hind > 0:
+    opilaspilet = input("Kas sul on õpilaspilet? (jah/ei: ").lower()
+    if opilaspilet == "jah":
+        hind = hind * 0.90
+        print("Sinu teatripileti hind on {:.2f} €.".format(hind))
+
