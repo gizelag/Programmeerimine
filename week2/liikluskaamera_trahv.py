4c6b9b3880d56bdb01336da720ccb38b47d06637
gunin <gizela@taltech.ee>
2025-09-16 17:01:17 +0300

    liitmine_lahutamine.py

--- week2/liikluskaamera_trahv.py
+++ week2/liikluskaamera_trahv.py
@@ -1,6 +1,9 @@
-kiirus = float(input("sisesta auto kiirus km/h:"))
-if kiirus < 50: print("lubatud kiirus")
-elif kiirus >= 80: print("kiirus! trahv summas: {trahvi_summa} EUR")
-else: print("hoiatus: ole ettevaatlik")
-uletatud_kiirus = - 80
-trahvi_summa = (uletatud_kiirus - 80) * 5
+kiirus = float(input("sisesta auto kiirus km/h: "))
+trahvi_summa = (kiirus - 80) * 5
+
+if kiirus < 50:
+    print("Lubatud kiirus")
+elif kiirus >= 80:
+    print(f"kiirus! trahv summas: {trahvi_summa} EUR")
+else:
+    print("hoiatus: ole ettevaatlik")
