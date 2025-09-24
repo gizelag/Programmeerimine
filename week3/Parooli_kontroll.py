293221691d598587343d83d5b38aecdcc1be3663
gunin <gizela@taltech.ee>
2025-09-24 12:50:15 +0300

    m

--- week3/Parooli_kontroll.py
+++ week3/Parooli_kontroll.py
@@ -1,39 +1,38 @@
 def on_piisavalt_pikk(parool):
     return len(parool) >= 8
-    count = 0
-    for sisaldab_tahti_ja_numbreid in parool:
-        count += 1
 
 def sisaldab_tahti_ja_numbreid(parool):
     sisaldab_tahti = False
     sisaldab_numbreid = False
-    for sisaldab_tahti_ja_numbreid in parool:
-        char = 'a'
+    for char in parool:
         if char.isalpha():
             sisaldab_tahti = True
-            elif char.isdigit():
+        if char.isdigit():
             sisaldab_numbreid = True
             print("See on number")
-            return sisaldab_tahti and sisaldab_numbreid
+    return sisaldab_tahti and sisaldab_numbreid
 
 def ei_sisalda_tuhikuid(parool):
-    for tuhik in parool:
-        if tuhik == " "
-            return False
+    for char in parool:
+        if char == " "
+        return False
         return True
 
  def main ():
     katsed = 5
+
     while katsed > 0:
         parool = input("Sisesta parool: ")
-        if ei_sisalda_tuhikuid(parool):
-            and sisaldab_tahti_ja_numbreid(parool):
+
+        if (ei_sisalda_tuhikuid(parool) and sisaldab_tahti_ja_numbreid(parool) and on_piisavalt_pikk(parool)):
             print("Parool aktsepteeritud.")
             return
-        katsed -= 1
-        if katsed > 0:
-            print("Parool ei vasta nõuetele. Katseid jäänud: {katsed}")
         else:
-            print("Liiga palju katseid. Ligipääs keelatud.")
+            katsed -= 1
+            if katsed > 0:
+                print(f"Parool ei vasta nõuetele. Alles on: {katsed} katset.")
+            else:
+                print("Liiga palju katseid. Ligipääs keelatud.")
+
 if __name__ == "__main__":
 
--- week3/arvude_analuus.py
+++ week3/arvude_analuus.py
@@ -1,12 +1,49 @@
 def leia_suurim(uus, senine_max):#vordleb kahte arvu ja tagastab suurema
-pass
-def kas_suurem_kui_50 (arv):
-    True kui arv > 50
-    False kui arv < 50
-pass
+    if uus > senine_max:
+        return uus
+    else:
+        return senine_max
+
+def kas_suurem_kui_50(arv):
+    return arv > 50
+
 def main():
+    print("Sisesta arvud ükshaaval (1-100) lõpetamiseks kirjuta 'lõpp'")
+    summa = 0
+    suurim = 0
+    yle_viiekymne = 0
+    arv_kokku = 0
+
     while True:
-        print("Sisesta arvud ükshaaval (1-100) lõpetamiseks kirjuta 'lõpp'")
-        kasutaja_sisend =
+        kasutaja_sisend = input("Sisesta arv(või kirjuta 'lõpp'): ").lower()
+        if kasutaja_sisend == "lõpp":
+            break
+        if not kasutaja_sisend.isdigit():
+            print("Viga: sisestus peab olema täisarv vahemikus 1 kuni 100.")
+            continue
+        arv = int(kasutaja_sisend)
+        if arv <1 or arv > 100:
+            print("Viga: arv peab olema vahemikus 1 kuni 100.")
+            continue
+
+        summa += arv
+        suurim = leia_suurim(arv, suurim)
+        arv_kokku += 1
+        if kas_suurem_kui_50(arv):
+            yle_viiekymne += 1
+
+            print("\n ---TULEMUSED---")
+        if arv_kokku == 0:
+            print("Ühtegi arvu ei sisestatud.")
+        else:
+            keskmine = summa / arv_kokku
+            print(f"Ssestati {arv_kokku} arvu.")
+            print(f"Arvude summa: {summa}")
+            print(f"Suure suurim: {suurim}")
+            print(f"Keskmine: {keskmine}")
+            print(f"Arvud, mis olid suuremad kui 50: {yle_viiekymne}")
+
+if __name__ == "__main__":
+    main()
 
 
