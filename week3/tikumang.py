f4f00ae5f57fe8dc4db038ff8b06385811f3ce19
gunin <gizela@taltech.ee>
2025-09-24 14:31:21 +0300

    m

---
+++ week3/tikumang.py
@@ -0,0 +1,69 @@
+import time
+import random
+
+def kusi_kasutaja_kaik(tikud_alles):
+    print("Mängija 1 kord(sul on 5 sekundit)")
+    start = time.time()
+    kasutaja_input = input("Mitu tikku võtad(1-5)?")
+    end = time.time()
+
+    kestus = end - start
+    if kestus > 5:
+        print("Aeg läbi! Võetakse auttomaatselt 1 tikk.")
+        return 1
+    if not kasutaja_input.isdigit():
+        print("Vigane sisend! võetakse automaatselt 1 tikk.")
+        return 1
+
+    mitu = int(kasutaja_input)
+    if mitu < 1 or mitu > 5:
+        print("Vale arv! Võetakse automaatselt 1 tikk. ")
+        return 1
+
+    if mitu > tikud_alles:
+        print("Pole nii palju tikke järel!Võetakse automaatselt 1 tikk.")
+        return 1
+
+    return mitu
+
+def arvuti_kaik(tikud_alles):
+    max_voetav = min(5, tikud_alles)#suurim lubatud vaartus
+    kaik = random.randint(1, max_voetav)
+    print("Mängija 2 (arvuti) kord:")
+    print(f"Arvuti: {kaik}")
+    return kaik
+
+def main():
+    tikud = 20
+    mangija = 1#1 on inimene ja 2 on arvuti
+
+    print("Tikumäng algab! Viimase tiku võtja kaotab.")
+    print(f"Tikke järel: {tikud} \n")
+
+    while tikud > 0:
+        if mangija == 1:
+            kaik = kusi_kasutaja_kaik(tikud)
+            print(f"Kasutaja käik: {kaik}")
+        else:
+            kaik = arvuti_kaik(tikud)
+
+        tikud -= kaik
+        if tikud < 0:
+            tikud = 0
+
+        print(f"Alles: {tikud} tikku")
+        if tikud == 0:
+            mangija = 1
+            print("\nMängija 1 (sina) võttis viimase tikuja kaotas")
+            print("Arvuti võitis!")
+        else:
+            print("\nArvuti võttis viimase tikuja kaotas")
+            print("Palju õnne, sa võitsid!")
+        break
+    print(f"Tikke järel: {tikud} \n")
+    if mangija == 1:
+        mangija = 2
+    else:
+        mangija = 1
+
+if __name__ == "__main__":
