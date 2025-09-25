1195594d8808eccab96100d3315a8f61cc4d8a38
gunin <gizela@taltech.ee>
2025-09-25 13:06:16 +0300

    m

--- week3/turtle_kujundid.py
+++ week3/turtle_kujundid.py
@@ -1,15 +1,15 @@
 import turtle
 
-def joonista_kolmnurk():"""joonistab punase kolmnurga asukohas -150, 0 ja kasutab for tsukklit"""
-
-
+def joonista_kolmnurk():
+    """joonistab punase kolmnurga asukohas -150, 0 ja kasutab for tsukklit"""
     turtle.goto(-150,0)
     turtle.color('red')
     for i in range(3):#kulgede arv 3
         turtle.forward(100)#100 uhikut
         turtle.left(120)#poordenurk 120
 
-def joonista_ruut():"""joonistab sinise ruudu asukohas -50, 0 ja kasutab for tsukklit"""
+def joonista_ruut():
+    """joonistab sinise ruudu asukohas -50, 0 ja kasutab for tsukklit"""
 
 
     turtle.goto(-50,0)
@@ -25,11 +25,13 @@ def joonista_viisnurk():
     turtle.goto(150,0)
     turtle.color('green')
 
-    for i in range(5):#5 kulge ja 250 uhikut ja 72kraadi
+    for i in range(5):
+        #5 kulge ja 250 uhikut ja 72kraadi
         turtle.forward(250)
         turtle.left(72)
 
-def main():#pohifunktsioon mis joonistab koik kujundid
+def main():
+    #pohifunktsioon mis joonistab koik kujundid
 
     turtle.speed(5)
     joonista_kolmnurk()
