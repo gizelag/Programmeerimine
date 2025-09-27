11a21cf17fc05b00de4cd3fc5026c84241bc48be
gunin <gizela@taltech.ee>
2025-09-27 20:55:08 +0300

    2

--- week3/turtle_muster.py
+++ week3/turtle_muster.py
@@ -2,38 +2,38 @@ import turtle
 
 #joonistab 3 ruutu iga jargmineon suurem ja rohkem pooratud
 def joonista_3_ruutu(alg_suurus = 30, kasv = 15, alg_nurk = 10, nurga_kasv = 20, varv = 'red'):
-   turtle.penup
-   turtle.goto(-100, 100)
-   turtle.pendown
-   turtle.color(varv)
+    # valine tsukkel 3 ruutu
+    for i in range(3):
+        suurus = alg_suurus + i * kasv
+        nurk = alg_nurk + i * nurga_kasv
+        turtle.setheading(nurk)
 
-   #valine tsukkel 3 ruutu
-   for i in range(3):
-       suurus = alg_suurus + i * kasv
-       nurk = alg_nurk + i * nurga_kasv
-       turtle.setheading(nurk)
+        turtle.penup
+        turtle.goto(-100, 100)
+        turtle.pendown
+        turtle.color(varv)
 
-        #sisemine tsukkel joonistab 4kulge
-       for i in range(4):
+        #sisemine
+        for i in range(4):
            turtle.forward(suurus)
            turtle.right(90)
 
 
 #joonistab 5 kolmnurka
 def joonista_5_kolmnurka(alg_suurus = 25, kasv = 15, alg_nurk = 5, nurga_kasv = 10, varv = 'purple'):
-
-    turtle.penup
-    turtle.goto(100, -100)
-    turtle.pendown
-    turtle.color(varv)
-
-    #valine tsukkel 5 kolmnurka
+    # valine tsukkel 5 kolmnurka
     for i in range(5):
         suurus = alg_suurus + i * kasv
         nurk = alg_nurk + i * nurga_kasv
-        turtle.setheading(nurk)#seab kilpkonna suuna tapselt maaratud nurga juurde
+        turtle.setheading(nurk)  # seab kilpkonna suuna tapselt maaratud nurga juurde
 
-        #sisemine tsukkel joonistab kolm nurka
+        turtle.penup
+        turtle.goto(100, -100)
+        turtle.pendown
+        turtle.color(varv)
+
+
+            #sisemine tsukkel joonistab kolm nurka
         for i in range(3):
             turtle.forward(suurus)
             turtle.left(120)
@@ -43,7 +43,7 @@ def main():
     turtle.speed(0)
     joonista_3_ruutu()
     joonista_5_kolmnurka()
-    turtle.done
+    turtle.done()
 #kutsun valja pohifunktsiooni
 if __name__ == '__main__':
     main()
