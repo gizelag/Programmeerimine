f2cbdabf5c507d2fcb7505da4b836eb8dbf2a16c
gunin <gizela@taltech.ee>
2025-10-01 12:54:10 +0300

    1

--- week4/voistluse_tulemused.py
+++ week4/voistluse_tulemused.py
@@ -1,44 +1,75 @@
 def loe_tulemused(failinimi):
     try:
-        with open(failinimi,"r", encoding="utf-8") as f:
+        with open(failinimi, 'r', encoding='utf-8') as f:
             return f.readlines()
     except FileNotFoundError:
-        print(f"Viga: Faili '{failinimi}' ei leitud ")
+        print(f"Viga: Faili `{failinimi}` ei leitud")
         return None
 
+
 def leia_voitja(failisisu):
+    # Kontrolli, ega funktsiooni sisend pole None
     if failisisu is None:
         return None, 0
+
+    voitja_nimi = ""
+    voitja_punktid = 0
+
     try:
-        for rida in failisisu(1):
-        rida.strip()
-        if not rida:
-            continue
-        if ";" not in rida:
+        for rida in failisisu:
+            # rida.strip()
+            # print(rida)
+
+            # Kui tuhi rida siis jatame vahele
+            if not rida:
                 continue
 
-        try:
-            rea_osad = rida.split(";")
-            if len(rea_osad) != 2:
+            # Kas eraldaja mark on, kui ei ole jatame vahele
+            if ";" not in rida:
                 continue
 
-            nimi = rea_osad[0].strip()
-            if not nimi:
+            try:
+                rea_osad = rida.split(";")
+                # print(rea_osad)
+
+                # Kontrolli, et tükeldamise tulemusena on täpselt 2 osa -> voib tekkida ValueError
+                if len(rea_osad) != 2:
+                    continue
+
+                # Leiame nime
+                nimi = rea_osad[0].strip()
+
+                # Kontrolli, et nimi ei ole tühi, kui on jäta vahele
+                if not nimi:
+                    # print(nimi)
+                    continue
+
+                # Teisendame punktid integeriks, praegu on see str -> siin voib tekkida ValueError
+                punktid = int(rea_osad[1].strip())
+
+                # Leia kõrgeima punktisummaga osaleja nimi ja tema punktid
+                if punktid > voitja_punktid:
+                    voitja_punktid = punktid
+                    voitja_nimi = nimi
+
+            # Jäta vigased read vahele ja jätka järgmise rea
+            except (ValueError, IndexError):
                 continue
-
-            punktid = int rea_osad[1].strip()
-
-            if punktid > voitja_punktid:
-                voitja_punktid = punktid
-                voitja_nimi = nimi
-
-
-            if not punktid:
-        except (ValueError, IndexError):
-            continue
 
         return voitja_nimi, voitja_punktid
+
     except Exception:
-        print(f"VÕITJA: {voitja_nimi}, {voitja_punktid}")
+        print("Error tekkis funktsioonis `leia_voitja(failisisu)`")
+        return None, 0
+
+
+def main():
+    sisu = loe_tulemused(failinimi="tulemused.txt")
+
+    nimi_voitjal, punktid_voitjal = leia_voitja(failisisu=sisu)
+
+    print(f"VÕITJA: {nimi_voitjal} ({punktid_voitjal} punkti)")
+
 
 if __name__ == "__main__":
+    main()
