86fd34ba322107468d3343ee5d8bfad8f40ce59e
gunin <gizela@taltech.ee>
2025-10-01 15:41:16 +0300

    4

--- week4/sugupuu.py
+++ week4/sugupuu.py
@@ -1,10 +1,8 @@
 def loe_pereandmed(failinimi):
-
     try:
         with open(failinimi,"r", encoding="utf-8") as fail:
             return fail.readlines()
-    except FileNotFoundError:
-        #print(f"Viga: '{failinimi}' ei leitud")
+    except FileNotFoundError:#kui ei leita viga
         return None
 
 
@@ -19,74 +17,81 @@ def kuva_viis_esimest(failisisu):
             if not ";" in rida:
                 continue
 
-        try:
-            rea_osad = rida.split(";")#tukeldan rea osad sobiva eraldajaga
-            if len(rea_osad) != 2:#kontollin et tukeldamise tulemusena on tapselt kaks rida
+            try:
+                rea_osad = rida.split(";")#tukeldan rea osad sobiva eraldajaga
+                if len(rea_osad) != 2:#kontollin et tukeldamise tulemusena on tapselt kaks rida
+                    continue
+
+                nimi = rea_osad[0].strip()#leiame nime
+                if not nimi:
+                    continue#kontrollime et peale tuhikute eemaldamist nimi ei oleks tuhi
+
+                vanus = int(rea_osad[1].strip())
+                if not vanus:
+                    continue
+                print(f"{nimi} on {vanus} aastat vana")
+
+            except (ValueError, IndexError):
                 continue
-            nimi = rea_osad[0].strip()#leiame nime
-            if not nimi:#kontrollime et nimi ei ole tuhi peale tuhikute eemaldamist
-                continue
 
-            vanus = int(round(rea_osad[1].strip()))
-            print(f"{nimi} on {vanus} aastat vana")
-
-
-        except (ValueError, IndexError):
-            continue
+    except Exception:
+        return None
 
 
 def arvuta_perestatistika(failisisu):
     if failisisu is None:
-        return None
+        return None,None,None
     inimeste_arv = 0
-    vanim= None
     vanus_kokku = 0
     noorim = None
+    vanim = None
 
     try:
         for rida in failisisu:
-            rida = rida.strip()
-            if not rida:continue
-
-            if not (";") in rida:
+            if not rida:
                 continue
 
-        try:
-            rea_osad = rida.split(";")  # tukeldan rea osad sobiva eraldajaga
-
-            if len(rea_osad) != 2:  # kontollin et tukeldamise tulemusena on tapselt kaks rida
+            if (";") not in rida:
                 continue
 
-            vanus = int(rea_osad[1].strip())  # leiame vanuse
+            try:
+                rea_osad = rida.split(";")  # tukeldan rea osad sobiva eraldajaga
 
-            if not vanus:  # kontrollime et vanus ei ole tuhi peale tuhikute eemaldamist
-                continue
-            inimeste_arv += 1
-            vanus_kokku += vanus
-            if vanim is None or vanus > vanim:
-                vanim = vanus
-            if noorim is None or vanus < noorim:
-                noorim = vanus
+                if len(rea_osad) != 2:  # kontollin et tukeldamise tulemusena on tapselt kaks rida
+                    continue
+                nimi = rea_osad[0].strip()
+                if not nimi:
+                    continue
 
-        except (ValueError, IndexError):
-            continue
+                vanus = int(rea_osad[1].strip())  # leiame vanuse
+                if vanus < 0:
+                    continue
+                inimeste_arv += 1
+                vanus_kokku += vanus
+                if vanim is None or vanus > vanim:
+                    vanim = vanus
+                if noorim is None or vanus < noorim:
+                    noorim = vanus
+            except (ValueError, IndexError):
+                vanus = 0
+        if inimeste_arv == 0:
+            return None, None,None
 
         keskmine = vanus_kokku / inimeste_arv
         keskmine = round(keskmine)
         return keskmine, noorim, vanim
+
     except Exception:
         return None
 
 def main():
-    sisu = loe_pereandmed(failinimi="vanused.txt")
+    sisu = loe_pereandmed(failinimi='vanused.txt')
     kuva_viis_esimest(failisisu=sisu)
-    tulemus = arvuta_perestatistika(failisisu=sisu)
-    if tulemus is not None:
-        keskmine_vanus, noorim_vanus, vanim_vanus = tulemus
-        print(f"Keskmine vanus:{int(keskmine_vanus)}")
-    if noorim_vanus is not None:
+    statistika = arvuta_perestatistika(failisisu=sisu)
+    if statistika is not None:
+        keskmine_vanus, noorim_vanus, vanim_vanus = statistika
+        print(f"Keskmine vanus:{keskmine_vanus}")
         print(f"Noorim vanus:{noorim_vanus}")
-    if vanim_vanus is not None:
         print(f"Vanim vanus:{vanim_vanus}")
     else:
         print("Ei suutnud leida ühtegi kehtivat vanust failist")
