f0d8be4597e0261dce4ef2e0acd2802c17ec0ecb
gunin <gizela@taltech.ee>
2025-10-08 22:12:27 +0300

    g

--- week5/nadala_temperatuurid.py
+++ week5/nadala_temperatuurid.py
@@ -7,22 +7,22 @@ def leia_paeva_keskmine(paevad, paevade_temp):
         return {}
 
     try:
-        paevade_kesk_dict = {
+        paevade_avg_dict = {
             paev: round(sum(tempid) / len(tempid), 1)
             for paev, tempid in zip(paevad, paevade_temp)#uhendame
         }
-        return paevade_kesk_dict
+        return paevade_avg_dict
     except Exception as e:
         print(f"Viga:{e}")
         return {}
 
 
-def leia_ekstreemsed(paevade_kesk_dict):
-    if not paevade_kesk_dict:
+def leia_ekstreemsed(paevade_avg_dict):
+    if not paevade_avg_dict:
         print("Viga, leia_ekstreemsed")
         return None, None
-    soojem= max(paevade_kesk_dict, key=lambda x: paevade_kesk_dict[x])
-    kulmem= min(paevade_kesk_dict, key=lambda x: paevade_kesk_dict[x])
+    soojem= max(paevade_avg_dict, key=lambda x: paevade_avg_dict[x])
+    kulmem= min(paevade_avg_dict, key=lambda x: paevade_avg_dict[x])
     return soojem, kulmem
 
 def main():
@@ -37,12 +37,12 @@ def main():
         [0.0, -0.5, -0.8, -1.0, -1.3],  # L
         [1.2, 1.9, 2.0, 2.5, 1.8]  # P
     ]
-    paevade_kesk_dict = leia_paeva_keskmine(paevad, paevade_temp)
-    print(f"Päevade keskmised: {paevade_kesk_dict}")
-    soojem, kulmem = leia_ekstreemsed(paevade_kesk_dict)
+    paevade_avg_dict = leia_paeva_keskmine(paevad, paevade_temp)
+    print(f"Päevade keskmised: {paevade_avg_dict}")
+    soojem, kulmem = leia_ekstreemsed(paevade_avg_dict)
     if soojem and kulmem:
-        print(f"Soojem keskmine: {soojem} ({paevade_kesk_dict[soojem]})")
-        print(f"Külmem keskmine: {kulmem} ({paevade_kesk_dict[kulmem]})")
+        print(f"Soojem keskmine: {soojem} ({paevade_avg_dict[soojem]})")
+        print(f"Külmem keskmine: {kulmem} ({paevade_avg_dict[kulmem]})")
 
 if __name__ == "__main__":
     main()
