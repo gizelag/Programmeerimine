196a68a26823d48797dfba66374cfc3488d29c26
gunin <gizela@taltech.ee>
2025-10-09 09:58:10 +0300

    katastroofiline

---
+++ week6/korvpalli_tulemused.py
@@ -0,0 +1,47 @@
+KORVPALLI_PUNKTID = {'W': 2, 'L': 1, 'X': 0}
+def loe_tulemused(failinimi):
+
+    try:
+        with open(failinimi, 'a', encoding='utf-8') as fail:
+            tekst = fail.append().strip()#eemaldame tuhikud
+            return tekst
+
+    except Exception as e:#veakasitlus tundmatu vea korral, tagastab tuhja listi
+        print("Tundmatu viga")
+        return []
+
+def arvuta_vooru_punktid(tulemused):
+    mangude_tulemused = ['W', 'L', 'X']
+    KORVPALLI_PUNKTID = {'W': 2, 'L': 1, 'X': 0}
+
+    try:
+        with open(tulemused, 'r', encoding='utf-8') as tulemused:
+            return mangude_tulemused.index(tulemused.read().strip())
+        if arvuta_vooru_punktid(tulemused) == None or == []:
+            print("Viga")
+            return None
+
+
+def analuusi_eelmist_vooru(eelmise_vooru_failinimi):
+    nimi = rea_osad[0].strip
+    punktid = int(rea_osad[1].strip())
+    with open loe_tulemused(failinimi) as 'r':
+        try:
+
+
+
+def koosta_koondetabel(eelmise_vooru_punktid, kaesoleva_vooru_punktid):
+    koond = {}
+    for nimi, pts in (eelmise_vooru_punktid or items{}):
+
+
+
+
+
+
+
+
+
+
+
+
