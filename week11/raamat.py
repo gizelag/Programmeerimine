4ac13ebcedf99fae0a747264262debb96c5d361c
gunin <gizela@taltech.ee>
2025-11-19 21:25:13 +0200

    m

---
+++ week11/raamat.py
@@ -0,0 +1,9 @@
+class Raamat():#loome klassi nimega raamat
+    def __init__(self, pealkiri:str, autor:str, lehekuljed:int, liik:str):
+        self.pealkiri = pealkiri    #lisame atribuudid
+        self.autor = autor
+        self.lehekuljed = lehekuljed
+        self.liik = liik
+
+    def __str__(self):
+        return f"{self.pealkiri}, {self.autor}, {self.lehekuljed}, {self.liik}"
